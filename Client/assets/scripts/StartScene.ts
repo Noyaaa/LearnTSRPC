6c546dd2a948e51f0b631e7110d26baf1090a612
LZZ <471845265@qq.com>
2022-03-28 17:09:50 +0800

    上传Cocos

---
+++ Client/assets/scripts/StartScene.ts
@@ -0,0 +1,10 @@
+
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+ 
+@ccclass('StartScene')
+export class StartScene extends Component {
+    start () {
+        
+    }
+}
